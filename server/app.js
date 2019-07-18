d00eed9bd7400700ad9fea021639d39a6b808795
莊育銘 <chming28@gmail.com>
2019-07-18 11:01:32 +0800

    init heroku postgresql

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const { Pool } = require('pg')
+require('dotenv').config()
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+})
 
 app.use(
   '/',
@@ -8,5 +15,18 @@ app.use(
     index: 'index.html'
   })
 )
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table')
+    const results = { results: result ? result.rows : null }
+    res.json(results)
+    client.release()
+  } catch (err) {
+    console.error(err)
+    res.send('Error ' + err)
+  }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
