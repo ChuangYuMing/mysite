43a4320b13d62f945c049e482ac650e632ab666c
莊育銘 <chming28@gmail.com>
2019-07-25 00:01:12 +0800

    add newrelic

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const port = process.env.PORT || 3000
 const { Pool } = require('pg')
 require('dotenv').config()
+require('newrelic')
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
