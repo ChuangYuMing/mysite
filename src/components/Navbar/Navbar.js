400e6f5e10a0b9246123edda684ca059d2f9c95a
jamie <jamie@pixnet.tw>
2019-07-25 17:58:48 +0800

    mobile menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,26 +8,72 @@ let cx = classNames.bind(styles)
 class Navbar extends Component {
   constructor() {
     super()
+    this.state = {
+      openMenu: false
+    }
     this.goHome = this.goHome.bind(this)
+    this.toggleMenu = this.toggleMenu.bind(this)
   }
 
   goHome() {
     this.props.history.push('/')
   }
 
+  toggleMenu() {
+    this.setState({
+      openMenu: !this.state.openMenu
+    })
+  }
+
   componentDidMount() {}
   componentWillUnmount() {}
 
   render() {
+    let { openMenu } = this.state
+    let menucx = cx({
+      category: true,
+      open: openMenu,
+      close: !openMenu
+    })
+
     return (
-      <div className={cx('wrapper')}>
-        <img className={cx('logo')} src={logo} onClick={this.goHome} />
-        <nav className={cx('category')}>
-          <Link to="/finance">Finance</Link>
-          <Link to="/technology">Technology</Link>
-          <Link to="/politics">Politics</Link>
-        </nav>
-      </div>
+      <>
+        <div className={cx('wrapper')}>
+          <img className={cx('logo')} src={logo} onClick={this.goHome} />
+          <nav className={cx('category')}>
+            <Link to="/finance">Finance</Link>
+            <Link to="/technology">Technology</Link>
+            <Link to="/politics">Politics</Link>
+          </nav>
+        </div>
+        <div className={cx('mb-wrapper')}>
+          <div className={cx('top')}>
+            <img className={cx('logo')} src={logo} onClick={this.goHome} />
+            <span className={cx('title')}>ChildBen</span>
+            <button
+              className={cx('menu')}
+              type="text"
+              onClick={this.toggleMenu}
+            >
+              menu
+            </button>
+          </div>
+          <nav className={menucx}>
+            <button
+              className={cx('menu')}
+              type="text"
+              onClick={this.toggleMenu}
+            >
+              menu2
+            </button>
+            <div classNames={cx('links')}>
+              <Link to="/finance">Finance</Link>
+              <Link to="/technology">Technology</Link>
+              <Link to="/politics">Politics</Link>
+            </div>
+          </nav>
+        </div>
+      </>
     )
   }
 }
