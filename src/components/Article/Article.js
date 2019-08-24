545eb377fc2370ce7d759f5af70055f5b13ec1f8
莊育銘 <chming28@gmail.com>
2019-08-24 15:26:06 +0800

    add questions json-ld

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -3,12 +3,11 @@ import styles from './article.css'
 import classNames from 'classnames/bind'
 import { connect } from 'react-redux'
 import { getArticleAsync } from '../../store/reducers/article'
-import { Helmet } from 'react-helmet'
 import { sendPageView } from '../../utils/tracking'
 import LoadingBtn from '../Common/LoadingBtn/LoadingBtn'
 import TopImage from './TopImage/TopImage'
-import javascript from 'highlight.js/lib/languages/javascript'
 import NotFound from '../NotFound/NotFound'
+import MetaTag from './MetaTag/MetaTag'
 
 let cx = classNames.bind(styles)
 
@@ -41,6 +40,9 @@ class Article extends Component {
   }
 
   render() {
+    let { title, content, url } = this.props.datas
+    let { url: browserUrl } = this.props.match.params
+
     if (url !== browserUrl || this.props.isFetching) {
       return (
         <article className={cx('wrapper')}>
@@ -53,21 +55,9 @@ class Article extends Component {
       return <NotFound />
     }
 
-    let {
-      title,
-      content,
-      description,
-      keywords,
-      url,
-      date,
-      category,
-      modified_time
-    } = this.props.datas
-    let { url: browserUrl } = this.props.match.params
     let isChrome =
       !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)
 
-    modified_time = modified_time ? modified_time : date
     return (
       <article className={cx('wrapper')}>
         <h1 className={cx('title')}>{title}</h1>
@@ -76,71 +66,7 @@ class Article extends Component {
           className={cx('content')}
           dangerouslySetInnerHTML={{ __html: content }}
         />
-        <Helmet>
-          <title>{title}</title>
-          <meta name="description" content={description} />
-          <meta name="keywords" content={keywords} />
-
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@ChildBen" />
-          <meta name="twitter:title" content={title} />
-          <meta name="twitter:description" content={description} />
-          <meta name="twitter:creator" content="@ChildBen" />
-          <meta
-            name="twitter:image:src"
-            content={`https://cdn.childben.com/${url}/${url}.jpg`}
-          />
-
-          <meta property="og:title" content={title} />
-          <meta property="og:type" content="article" />
-          <meta
-            property="og:url"
-            content={`https://www.childben.com/${category}/${url}`}
-          />
-          <meta
-            property="og:image"
-            content={`https://cdn.childben.com/${url}/${url}.jpg`}
-          />
-          <meta property="og:description" content={description} />
-          <meta property="og:site_name" content="ChildBen" />
-          <meta property="article:published_time" content={date} />
-          <meta property="article:modified_time" content={modified_time} />
-          <meta property="fb:admins" content="100031410993377" />
-
-          <script type="application/ld+json">
-            {`
-              {
-                "@context": "https://schema.org",
-                "@type": "BlogPosting",
-                "headline": "${title}",
-                "image": "https://cdn.childben.com/${url}/${url}.jpg",
-                "editor": "Child Ben",
-                "keywords": "${keywords}",
-                "url": "https://www.childben.com/${category}/${url}",
-                "datePublished": "${date}",
-                "dateCreated": "${date}",
-                "dateModified": "${modified_time}",
-                "description": "${description}",
-                "publisher": {
-                  "@type": "Organization",
-                  "name": "Blog Name",
-                  "url": "https://www.childben.com",
-                  "logo": {
-                  	"@type": "ImageObject",
-                    "url": "https://www.childben.com/static/images/android-icon-96x96.png",
-                    "width": 96,
-                    "height": 96
-                  }
-                },
-                "mainEntityOfPage": "https://www.childben.com/${category}/${url}",
-                "author": {
-                  "@type": "Person",
-                  "name": "ChildBen"
-                }
-              }
-          `}
-          </script>
-        </Helmet>
+        <MetaTag data={this.props.datas} />
       </article>
     )
   }
---
+++ src/components/Article/MetaTag/MetaTag.js
@@ -0,0 +1,111 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+function MetaTag({ data }) {
+  let {
+    title,
+    description,
+    keywords,
+    url,
+    date,
+    category,
+    modified_time,
+    questions
+  } = data
+  modified_time = modified_time ? modified_time : date
+
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@ChildBen" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:creator" content="@ChildBen" />
+      <meta
+        name="twitter:image:src"
+        content={`https://cdn.childben.com/${url}/${url}.jpg`}
+      />
+
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="article" />
+      <meta
+        property="og:url"
+        content={`https://www.childben.com/${category}/${url}`}
+      />
+      <meta
+        property="og:image"
+        content={`https://cdn.childben.com/${url}/${url}.jpg`}
+      />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content="ChildBen" />
+      <meta property="article:published_time" content={date} />
+      <meta property="article:modified_time" content={modified_time} />
+      <meta property="fb:admins" content="100031410993377" />
+
+      <script type="application/ld+json">
+        {`
+              {
+                "@context": "https://schema.org",
+                "@type": "BlogPosting",
+                "headline": "${title}",
+                "image": "https://cdn.childben.com/${url}/${url}.jpg",
+                "editor": "Child Ben",
+                "keywords": "${keywords}",
+                "url": "https://www.childben.com/${category}/${url}",
+                "datePublished": "${date}",
+                "dateCreated": "${date}",
+                "dateModified": "${modified_time}",
+                "description": "${description}",
+                "publisher": {
+                  "@type": "Organization",
+                  "name": "Blog Name",
+                  "url": "https://www.childben.com",
+                  "logo": {
+                  	"@type": "ImageObject",
+                    "url": "https://www.childben.com/static/images/android-icon-96x96.png",
+                    "width": 96,
+                    "height": 96
+                  }
+                },
+                "mainEntityOfPage": "https://www.childben.com/${category}/${url}",
+                "author": {
+                  "@type": "Person",
+                  "name": "ChildBen"
+                }
+              }
+          `}
+      </script>
+      {questiosJsonLd(questions)}
+    </Helmet>
+  )
+}
+
+function questiosJsonLd(questions) {
+  if (!questions) {
+    return
+  }
+
+  let mainEntity = JSON.parse(questions).map(item => {
+    let { name, text } = item
+    return {
+      '@type': 'Question',
+      name,
+      acceptedAnswer: {
+        '@type': 'Answer',
+        text
+      }
+    }
+  })
+  let data = {
+    '@context': 'https://schema.org',
+    '@type': 'FAQPage',
+    mainEntity
+  }
+  return <script type="application/ld+json">{JSON.stringify(data)}</script>
+}
+
+export default MetaTag
